da939f18e308f86853264989f432d2be94676b79
Deepak Belbase <deepak.belbase4@gmail.com>
2018-06-10 18:13:44 +0530

    Added: Particle effect in the Background

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,15 +7,18 @@ jQuery(function ($) {
     // --------------------------------------------------------------------
 
     (function () {
-        $('#preloader').delay(200).fadeOut('slow');
+      $.getJSON("assets/json/particle.json",function(data){
+        console.log(data);
+        particlesJS('particles-js',data);
+      });
+        $('#preloader').delay(500).fadeOut('slow');
     }());
-
-
-
+    
     // --------------------------------------------------------------------
     // Sticky Sidebar
     // --------------------------------------------------------------------
 
     $('.left-col-block, .right-col-block').theiaStickySidebar();
+
 
 }); // JQuery end
