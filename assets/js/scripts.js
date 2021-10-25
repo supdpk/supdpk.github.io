0c943d07abbcc863285477c1df18e352aad07b79
Deepak Belbase <deepak34@live.in>
2021-10-25 07:40:12 +0530

    Initialte the project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ jQuery(function ($) {
 
     (function () {
       $.getJSON("assets/json/particle.json",function(data){
-        console.log(data);
+        // console.log(data);
         particlesJS('particles-js',data);
       });
         $('#preloader').delay(500).fadeOut('slow');
@@ -22,3 +22,40 @@ jQuery(function ($) {
 
 
 }); // JQuery end
+$(($)=>{
+  $.ajax({
+    url:'/data.json',
+    method:'GET',
+    success:(res)=>{
+      console.log(res);
+      console.log(res);
+      $('.name').html(res.name);
+      $('.lead').html(res.title);
+      $('.about-text').html(res.aboutMe);
+      $('.profile-pic').prop('src',res.profilePic);
+      console.log(!res.cv);
+      console.log(!res.cv || res.cv=='');
+      if(!res.cv || res.cv==''){
+        $('.cv').hide();
+      }
+      else{
+        $('.cv').prop('href',res.cv);
+      }
+
+      if(!res.cv || res.linkedin==''){
+        $('.linkedin').hide();
+      }
+      else{
+        $('.linkedin').prop('href',res.linkedin);
+      }
+
+      if(!res.github || res.github==''){
+        $('.github').hide();
+      }
+      else{
+        $('.github').prop('href',res.github);
+      }
+      
+    }
+  })
+});
